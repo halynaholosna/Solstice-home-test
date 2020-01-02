23d18b9cc10b5deb16b0846382a6a5b96b28a635
halynaholosna <golosnayagalina@gmail.com>
2020-01-01 22:12:41 -0600

    Steps on the main page are passed

--- features/steps/main_page_steps.py
+++ features/steps/main_page_steps.py
@@ -1,5 +1,9 @@
-from behave import given
+from behave import given, when
 
 @given ('Open Amazon page')
 def open_amazon(context):
     context.driver.get('https://www.amazon.com/')
+
+@when ('Search for {product}')
+def search_product(context, product):
+    context.app.main_page.search_for_keyword(product)
--- pages/main_page.py
+++ pages/main_page.py
@@ -2,7 +2,8 @@ from selenium.webdriver.common.by import By
 from pages.base_page import Page
 
 class MainPage(Page):
-    #SEARCH_INPUT = (By.)
+    SEARCH_INPUT = (By.ID,'twotabsearchtextbox')
+    SEARCH_ICON = (By.CSS_SELECTOR, "input.nav-input[type='submit']")
 
     def search_for_keyword(self, text):
         self.input_text(text, *self.SEARCH_INPUT)
